e349e5967c74b0cd98e6ff60168b499e27dd2af8
Nelet <dogorii@naver.com>
2026-06-17 15:17:09 +0900

    [Passed] 사칙연산 함수 (2ms, 0MB)

---
+++ trail2/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void Add(int a, int c)
+{
+    cout << a << " + " << c << " = " << a + c;
+}
+
+void Sub(int a, int c)
+{
+    cout << a << " - " << c << " = " << a - c;
+}
+
+void Div(int a, int c)
+{
+    if(c == 0)
+    {
+        cout << "A cannot be divided by zero";
+    }
+    
+    cout << a << " / " << c << " = " << a / c;
+}
+
+void Mul(int a, int c)
+{
+    cout << a << " * " << c << " = " << a * c;
+}
+
+void Calculate(int a, char o, int c)
+{
+    switch(o)
+    {
+        case '+': Add(a, c); break;
+        case '-': Sub(a, c); break;
+        case '/': Div(a, c); break;
+        case '*': Mul(a, c); break;
+        default : cout << "False";
+    }
+}
+
+int main() {
+    // Please write your code here.
+    int a, c;
+    char o;
+
+    cin >> a >> o >> c;
+
+    Calculate(a, o, c);
+
+    return 0;
+}
